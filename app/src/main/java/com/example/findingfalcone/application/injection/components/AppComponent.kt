b381dbd2b70df3db4641dbf4c35657702fb57afa
amirRudd <amir.abdi@foxsports.com.au>
2020-03-27 17:43:43 +1100

    add Network module

--- app/src/main/java/com/example/findingfalcone/application/injection/components/AppComponent.kt
+++ app/src/main/java/com/example/findingfalcone/application/injection/components/AppComponent.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.findingfalcone.application.MyApp
 import com.example.findingfalcone.application.injection.modules.ApplicationModule
 import com.example.findingfalcone.application.injection.modules.MainModule
+import com.example.findingfalcone.application.injection.modules.NetworkModule
 import com.example.findingfalcone.application.injection.modules.ViewModelModule
 import dagger.Component
 import dagger.android.AndroidInjectionModule
@@ -12,12 +13,13 @@ import javax.inject.Singleton
 
 @Singleton
 @Component(
-        modules = [
-            AndroidInjectionModule::class,
-            AndroidSupportInjectionModule::class,
-            ApplicationModule::class,
-            MainModule::class,
-            ViewModelModule::class]
+    modules = [
+        AndroidInjectionModule::class,
+        AndroidSupportInjectionModule::class,
+        ApplicationModule::class,
+        MainModule::class,
+        ViewModelModule::class,
+        NetworkModule::class]
 )
 interface AppComponent {
 
